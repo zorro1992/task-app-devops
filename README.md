747219385e96488d04cc4dbe588cddbb98389d6c
raghu-de <rsan@hellofresh.com>
2021-08-17 16:29:53 +0200

    Small touches

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Task app
 
 Simple python task app that is used to explain the DevOps process.
+
+### Docker run
+
+To use docker directly use the below command
+```
+docker run -it --rm -p 127.0.0.1:5000:5000 rk045971/task-app-devops:main
+```
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model): # pylint: disable=too-few-public-methods
+class Todo(db.Model):
     """A dummy docstring."""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -26,21 +26,21 @@ class Todo(db.Model): # pylint: disable=too-few-public-methods
         """A dummy docstring."""
         print("")
 
-
+# Edit endpoint
 @app.route("/edit")
 def home1():
     """A dummy docstring."""
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
-
+# Default home endpoint
 @app.route("/")
 def list1():
     """A dummy docstring."""
     todo_list = Todo.query.all()
     return render_template("list.html", todo_list=todo_list)
 
-
+# Add endpoint
 @app.route("/add", methods=["POST"])
 def add():
     """A dummy docstring."""
@@ -50,7 +50,7 @@ def add():
     db.session.commit()
     return redirect(url_for("home1"))
 
-
+# Update endpoint
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     """A dummy docstring."""
@@ -59,7 +59,7 @@ def update(todo_id):
     db.session.commit()
     return redirect(url_for("home1"))
 
-
+# Delete endpoint
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     """A dummy docstring."""
@@ -68,7 +68,7 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home1"))
 
-
+# Main function
 if __name__ == "__main__":
     db.create_all()
     app.run(host="0.0.0.0", debug=True)
