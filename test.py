aa3d6277aba16d57e678a52fe77190347845e287
raghu-de <rsan@hellofresh.com>
2021-08-17 13:20:46 +0200

    Adding test case

---
+++ test.py
@@ -0,0 +1,17 @@
+from app import app
+
+def test_case_1():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+
+def test_case_2():
+    response = app.test_client().get('/edit')
+    assert response.status_code == 200
+
+
+def test_case_3():
+    response = app.test_client().get('/edit')
+    assert b"To Do App" in response.data
+    assert b"Todo Title" in response.data
+    assert b"Add" in response.data
